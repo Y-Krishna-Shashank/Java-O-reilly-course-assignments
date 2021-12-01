38438c2a232781b45c3acbe2b7189fdc1fe8df75
Y-Krishna-Shashank <94391991+Y-Krishna-Shashank@users.noreply.github.com>
2021-12-01 18:48:02 +0530

    Section 4 assignments

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myArray;
+        myArray = new int[5];
+        myArray[0]=1;
+        myArray[1]=22;
+        myArray[3]=4;
+        myArray[4]=14;
+        for(int i=0;i<myArray.length;i++)
+        {
+            System.out.println(myArray[i]);
+        }
+    }
+}
