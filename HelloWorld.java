3ffc55182b6d6d61881fa55fe9c8f5e392170e07
Y-Krishna-Shashank <94391991+Y-Krishna-Shashank@users.noreply.github.com>
2021-12-02 09:55:06 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-  import java.util.Arrays   ;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello Krishna Shashank!");
-    }
-}
